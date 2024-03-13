8888bc5c294d1a657c917933cb950a287a5db770
Taofiq <animashauntaofiq@gmail.com>
2024-03-14 00:23:53 +0100

    refactor: refactored topbar to follow UI

--- components/Navigation/Topbar.vue
+++ components/Navigation/Topbar.vue
@@ -1,39 +1,36 @@
 <script setup lang="ts">
 import UiBtn from "~/components/Ui/Btn/index.vue";
 import useTheme from "~/composables/useTheme";
-defineEmits(["toggleSidebar"]);
-const { setTheme } = useTheme();
-defineProps<{
-  isNavOpen: boolean;
+defineEmits<{
+  (e: "toggleSidebar"): void;
 }>();
-const { tooltipStyle } = usePvStyle();
-const sidebarToggler = ref<InstanceType<typeof UiBtn> | null>(null);
-useShortcut({
-  toggle() {
-    sidebarToggler.value?.triggerClick();
-  },
-});
+const { setTheme } = useTheme();
+const { toggleSidebar } = useMainStore();
+defineProps<{
+  title: string;
+}>();
 </script>
 
 <template>
   <div
-    class="flex items-center main sticky top-0 w-full h-[60px] z-30 shadow-sm bg-white dark:bg-darkBgSec"
+    class="flex items-center sticky top-0 w-[inherit] h-[var(--sidebar-height)] z-30 bg-bg-topbar dark:bg-bg-darkTopbar border-b border-b-border-topbar dark:border-b-border-darkTopbar"
   >
     <div class="p-4 w-full">
       <div class="flex justify-between items-center w-full">
         <div class="flex items-center gap-2">
           <UiBtn
-            v-tooltip="{
-              value: `${!isNavOpen ? 'collapse [' : 'expand ['}`,
-              pt: tooltipStyle,
-            }"
             ref="sidebarToggler"
-            class="!p-0 !bg-transparent"
-            @click="$emit('toggleSidebar')"
+            class="!p-0 !bg-transparent md:hidden"
+            @click="toggleSidebar"
           >
             <Icon name="mdi:menu" class="text-3xl font-bold cursor-pointer"></Icon>
           </UiBtn>
-          <p>Welcome, Blade!</p>
+          <span
+            class="rounded-full bg-bg-sidebarLink dark:bg-bg-darkSidebarLink text-text-primary dark:text-text-dark flex justify-center items-center w-8 h-8 cursor-pointer"
+          >
+            <Icon name="mdi:pound" width="15" />
+          </span>
+          <p class="font-bold text-text-primary dark:text-text-dark">{{ title }}</p>
         </div>
         <div class="flex items-center gap-4">
           <Icon
@@ -44,15 +41,8 @@ useShortcut({
             class="cursor-pointer"
             @click="setTheme($colorMode.preference === 'dark' ? 'light' : 'dark')"
           />
-          <Icon name="mdi:magnify" size="20" class="cursor-pointer" />
-          <Icon name="mdi:bell-outline" size="20" class="cursor-pointer" />
           <div class="flex gap-2 items-center cursor-pointer">
-            <img
-              src="https://avatars.githubusercontent.com/u/47092407?v=4"
-              alt="illustration_01"
-              class="w-10 h-10 border-2 border-primary rounded-full"
-            />
-            <p class="hidden md:flex">Blade</p>
+            <img src="~/assets/svg/members.svg" alt="members_avatar" />
           </div>
         </div>
       </div>
