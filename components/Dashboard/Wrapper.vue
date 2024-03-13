b2fd82ccdb1ac9a7901134388b56b1f873185336
Taofiq <animashauntaofiq@gmail.com>
2024-03-14 00:23:34 +0100

    feat: add dashboard wrapper

---
+++ components/Dashboard/Wrapper.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<template>
+  <div>
+    <NavigationTopbar :title="title" />
+    <div class="p-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
