19a83c49ba0015bc389a2952e74c85e61078513b
Taofiq <animashauntaofiq@gmail.com>
2024-03-14 00:23:00 +0100

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,90 @@
   theme: {
     extend: {
       colors: {
-        primary: '#eef1f8',
-        secondary: '#213547',
-        priBg: '#F5E6FE',
-        priText: '#35AF8A',
-        darkBg: '#121316',
-        darkBgSec: '#27293D',
-        btn: '#29292C'
+        btn: {
+          primary: '#EEEEEE',
+          secondary: '#5047EB',
+          dark: '#29292C',
+          darkSec: '#27293D',
+        },
+        text: {
+          primary: '#040404',
+          secondary: '#213547',
+          dark: '#ffffff',
+          darkSec: 'rgba(255, 255, 255, 0.6)',
+          placeholder: "rgba(4, 4, 4, 0.54)",
+          darkPlaceholder: "rgba(255, 255, 255, 0.54)",
+          disabled: "#d1d5db",
+          darkDisabled: "#1E1E1E",
+          focus: "#374151",
+          darkFocus: "#1E1E1E",
+          link: "#5047EB",
+          darkLink: "#5047EB",
+          hover: "#5047EB",
+          darkHover: "#5047EB",
+          label: "#374151",
+          darkLabel: "#ffffff",
+        },
+        border: {
+          primary: '#EEEEEEC',
+          secondary: '#213547',
+          dark: '#3A3A3A',
+          darkSec: '#27293D',
+          focus: '#374151',
+          darkFocus: 'rgba(255, 255, 255, 0.8)',
+          disabled: '#d1d5db',
+          darkDisabled: '#1E1E1E',
+          topbar: '#e4e4e4',
+          darkTopbar: '#2F2E31',
+        },
+        bg: {
+          primary: '#FAFAFA',
+          secondary: '#5047EB',
+          dark: '#121316',
+          darkSec: '#1C1D21',
+          focus: '#374151',
+          darkFocus: '#1E1E1E',
+          disabled: '#f9fafb',
+          darkDisabled: '#1E1E1E',
+          thumb: "#d8d8d8",
+          darkThumb: "#fefefe",
+          track: "#f9fafb",
+          darkTrack: "#27293D",
+          channelBar: "#f9fafb",
+          darkChannelBar: "#1C1D21",
+          sidebar: "#f9fafb",
+          darkSidebar: "#1A1B1E",
+          topbar: "#f9fafb",
+          darkTopbar: "#191A1D",
+          input: "#eeeeee",
+          darkInput: "#29292c",
+          sidebarLink:'#EEEEEE',
+          darkSidebarLink:'#27292D',
+        },
+        hover: {
+          sidebar: '#00000042'
+        },
+        dark: {
+          primary: '#121316',
+          secondary: '#27293D',
+        },
+        light: {
+          primary: '#eef1f8',
+          secondary: '#213547',
+        },
+        dropdown: {
+          checkbox: '#bbbbbbb3',
+          darkCheckbox: '#64748b',
+        },
+        file: {
+          dark: '#d8d8d8'
+        },
+        accent: {
+          primary: '#27293D',
+          secondary: '#FFD700',
+          dark: '#64748b',
+          darkSec: '#27293D',
+        },
       },
       fontFamily: {
         satoshi: ['Satoshi', 'sans-serif']
@@ -28,7 +105,7 @@
         '1xs': '350px',
         '1xl': '1400px',
         '3xl': '2000px'
-      }
+      },
     }
   },
   content: [
