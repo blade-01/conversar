82e1663c702830db4d984ca8061d079125d89d65
Taofiq <animashauntaofiq@gmail.com>
2024-03-14 00:25:11 +0100

    refactor: refactored auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware(() => {
-  const isAuthenticated = false;
+  const isAuthenticated = true; // Replace with your authentication logic
   // If the user is not authenticated, redirect to the auth page
   if (!isAuthenticated) {
     return navigateTo({
