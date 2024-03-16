ed3931eb61956810388388c3be10c3b196c55168
Taofiq <animashauntaofiq@gmail.com>
2024-03-17 00:13:35 +0100

    feat: add property styling to index

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,7 @@ definePageMeta({
 <template>
   <DashboardWrapper title="Introduction" description="Hello there, let's create magic 🪄">
     <div>
+      <!-- DATE -->
       <div class="flex justify-between items-center gap-5 text-center mb-5">
         <hr
           class="border-t border-t-border-topbar dark:border-t-border-darkTopbar basis-full"
@@ -20,8 +21,31 @@ definePageMeta({
           class="border-t border-t-border-topbar dark:border-border-darkTopbar basis-full"
         />
       </div>
-      <div v-for="i in 150" :key="i">
-        <h1 class="text-xl text-style mb-1">Welcome to Channel</h1>
+      <!-- ./ DATE -->
+
+      <div class="mt-5 flex flex-col gap-5">
+        <!-- MESSAGE DATA -->
+        <div class="flex items-start gap-2.5" v-for="i in 10" :key="i">
+          <PvAvatar
+            image="https://avatars.githubusercontent.com/u/47092407?v=4"
+            shape="circle"
+            class="w-10 h-10"
+          />
+          <div class="flex flex-col gap-1">
+            <div class="flex items-end gap-1">
+              <p class="text-style text-sm font-medium">Robert Kiyasoki</p>
+              <Icon name="mdi:circle" size="4" class="mb-2.5 text-style" />
+              <p class="text-[10px] font-light text-style">3:55AM</p>
+            </div>
+            <!-- MESSAGE -->
+            <p class="text-style text-sm">
+              Popular tools like Figma, Sketch, and Adobe XD are great for UI/UX design.
+              Experiment to find the one that suits your workflow.
+            </p>
+            <!-- ./ MESSAGE -->
+          </div>
+        </div>
+        <!-- ./ MESSAGE DATA -->
       </div>
     </div>
   </DashboardWrapper>
