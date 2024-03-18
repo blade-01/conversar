a3103b509bfae6a6da503df827715ad3924ab7bf
Taofiq <animashauntaofiq@gmail.com>
2024-03-18 10:54:57 +0100

    feat: updated auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,4 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  // const isAuthenticated = true; // Replace with your authentication logic
-  // // If the user is not authenticated, redirect to the auth page
-  // if (!isAuthenticated) {
-  //   return navigateTo({
-  //     name: "auth"
-  //   });
-  // }
-
+export default defineNuxtRouteMiddleware(async (to) => {
   const user = await getCurrentUser();
 
   // redirect the user to the login page
