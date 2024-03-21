a15f6097a3cbfc5659793e1f68f122fec8be0702
Taofiq <animashauntaofiq@gmail.com>
2024-03-21 02:17:53 +0100

    feat: refactored auth layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -4,7 +4,7 @@ const { setTheme } = useTheme();
 
 <template>
   <div class="flex flex-col justify-center gap-2.5 text-center px-4">
-    <div class="fixed top-5 right-5">
+    <div class="fixed top-5 right-5 lg:top-10 lg:right-10">
       <Icon
         :name="$colorMode.value === 'dark' ? 'mdi:weather-night' : 'mdi:weather-sunny'"
         size="23"
