b00bbf5958562ef3312904178bd6e40f3fb9c1b8
Taofiq <animashauntaofiq@gmail.com>
2024-03-21 02:16:30 +0100

    feat: updated error page UI

--- error.vue
+++ error.vue
@@ -13,9 +13,9 @@ const { setTheme } = useTheme();
 
 <template>
   <div
-    class="flex justify-center items-center flex-col max-w-sm mx-auto text-center h-screen overflow-hidden bg-bg-primary dark:bg-bg-dark"
+    class="flex justify-center items-center flex-col max-w-lg mx-auto text-center h-screen overflow-hidden bg-bg-primary dark:bg-bg-dark p-6"
   >
-    <div class="fixed top-5 right-5">
+    <div class="fixed top-5 right-5 lg:top-10 lg:right-10">
       <Icon
         :name="$colorMode.value === 'dark' ? 'mdi:weather-night' : 'mdi:weather-sunny'"
         size="23"
@@ -23,10 +23,28 @@ const { setTheme } = useTheme();
         @click="setTheme($colorMode.preference === 'dark' ? 'light' : 'dark')"
       />
     </div>
-    <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
-    <p class="mt-7">{{ error.message }}</p>
+    <div>
+      <img src="~/assets/svg/404-dark.svg" alt="404" v-if="$colorMode.value === 'dark'" />
+      <img
+        src="~/assets/svg/404-light.svg"
+        alt="404"
+        v-if="$colorMode.value === 'light'"
+      />
+    </div>
+    <div class="flex flex-col gap-5 items-center mt-7 text-style">
+      <p class="text-3xl lg:text-5xl font-bold">Oops!</p>
+      <p class="text-base md:text-lg">
+        We can't seem to find the page you are looking for.
+      </p>
+    </div>
     <div class="flex justify-center mt-8">
-      <UiBtn append-icon="bx:home-alt" @click="handleClearError"> Go Home </UiBtn>
+      <UiBtn
+        append-icon="bx:home-alt"
+        @click="handleClearError"
+        class="!bg-bg-secondary !text-white"
+      >
+        Go Home
+      </UiBtn>
     </div>
   </div>
 </template>
