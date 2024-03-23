5fb69cd0b4b4ee15a21a430643aabffbb82a48ec
Taofiq <animashauntaofiq@gmail.com>
2024-03-23 23:34:45 +0100

    feat: added members validation

--- composables/useValidations.ts
+++ composables/useValidations.ts
@@ -1,5 +1,4 @@
 import { object, string, ref as yref, array, date, mixed } from "yup";
-import { isValidPhoneNumber } from "libphonenumber-js";
 export default function () {
   // Validation messages
   const validation = {
@@ -18,121 +17,19 @@ export default function () {
 
   // Validation schema
   const mainSchema = object({
-    first_name: string()
-      .matches(/^[^\d]*$/, validation.alphabet("First name"))
-      .required(validation.required("First name"))
-      .min(1)
-      .max(100),
-    last_name: string()
-      .matches(/^[^\d]*$/, validation.alphabet("Last name"))
-      .required(validation.required("Last name"))
-      .min(1, validation.min("Last name", 1))
-      .max(100, validation.max("Last name", 100)),
-    email: string()
-      .required(validation.required("Email"))
-      .email(validation.required("Email"))
-      .matches(
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        validation.required("Email")
-      ),
-    portfolio: string()
-      .url(validation.url("Portfolio"))
-      .required(validation.required("Portfolio")),
-    currency: string()
-      .required(validation.required("Currency"))
-      .min(1, validation.min("Currency", 1)),
-    // Make sure to include at least 8 characters with at least one letter and one number
-    password: string()
-      .required(validation.password)
-      .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={}[\]:";'<>,.?\\/|!"#$%&'()*+,-./:;<=>?@^_`{|}~])(?=.{8,})/,
-        validation.password
-      ),
-    password_confirmation: string()
-      .required(validation.password_confirmation)
-      .oneOf([yref("password")], validation.password_confirmation),
-    message: string()
-      .required(validation.required("Message"))
-      .min(1, validation.min("Message", 1))
-      .max(100, validation.max("Message", 100)),
-    gender: string().required(validation.required("Gender")),
-    country: array()
-      .of(string().required())
-      .test(
-        "length",
-        validation.required("Country"),
-        (value: any) => value.length > 0
-      )
-      .required(validation.required("Country")),
-    city: array()
-      .of(string().required())
-      .test(
-        "length",
-        validation.required("Country"),
-        (value: any) => value.length > 0
-      )
-      .required(validation.required("City")),
-    date: date()
-      .required(validation.required("Date"))
-      .typeError(validation.valid("Date")),
-    month: date()
-      .required(validation.required("Month"))
-      .typeError(validation.valid("Month")),
-    year: date()
-      .required(validation.required("Year"))
-      .typeError(validation.valid("Year")),
-    date_time: date()
-      .required(validation.required("Date Time"))
-      .typeError(validation.valid("Date Time")),
-    time: date()
-      .required(validation.required("Time"))
-      .typeError(validation.valid("Time")),
-    multiple: array()
-      .of(date().required())
-      .required(validation.required("Multiple Dates"))
-      .test(
-        "length",
-        validation.required("Multiple Dates"),
-        (value: any) => value.length > 0
-      )
-      .typeError(validation.valid("Multiple Dates")),
-    range: array()
-      .of(date().required())
-      .required(validation.required("Date Range"))
-      .test(
-        "length",
-        validation.required("Date Range"),
-        (value: any) => value.length > 0
-      )
-      .typeError(validation.valid("Date Range")),
-    phone_number: string().test(
-      "phone",
-      validation.valid("Phone Number"),
-      (value) => {
-        if (!value) return true;
-        return isValidPhoneNumber(value);
-      }
-    ),
-    autocomplete: string().required(validation.required("Autocomplete")),
-    chips: string().required(validation.required("Chips")),
-    file: mixed().required(validation.required("File")),
-    files: array()
+    users: array()
       .of(mixed().required())
       .test(
         "length",
-        validation.required("Files"),
+        "Channel should contain at least 1 member",
         (value: any) => value.length > 0
       )
-      .required(validation.required("Files")),
-    image: mixed().required(validation.required("File")),
-    images: array()
-      .of(mixed().required())
       .test(
-        "length",
-        validation.required("Images"),
-        (value: any) => value.length > 0
+        "max",
+        "Channel should not exceed 2 members",
+        (value: any) => value.length <= 2
       )
-      .required(validation.required("Images"))
+      .required(validation.required("Members field"))
   });
 
   return { mainSchema };
