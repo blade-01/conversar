88b6af8b02e9e0a1ed26929b9c3cf4b097451563
Taofiq <animashauntaofiq@gmail.com>
2024-03-26 15:26:45 +0100

    re-fix: working on scroll bug

--- components/Dashboard/Wrapper.vue
+++ components/Dashboard/Wrapper.vue
@@ -59,7 +59,7 @@ const {
               <span class="capitalize">{{ title || "..." }}</span> channel
             </p>
           </div>
-          <div class="mt-5 mb-[30%] md:mb-0">
+          <div class="mt-5 mb-[40%] md:mb-0">
             <slot />
           </div>
         </div>
